2b24e4f68e35834e7e5c807857c2eb9c41e58aa9
asrafkaizen <asrafkaizen@gmail.com>
2019-11-17 16:20:14 +0800

    added template files

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/testing', function () {
     echo 'This is Testing';
@@ -35,3 +35,5 @@ Route::group([
     Route::post('/edit/{blog}', 'BlogController@update')->name('update');
     Route::get('/delete/{blog}', 'BlogController@delete')->name('delete');
 });
+
+Route::view('/','template.home');
